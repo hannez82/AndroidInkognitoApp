9f7808dc25c1ffcdf5ba60e3476aa1793bb27e0a
Hannes <hannes@bjurek.se>
2015-01-25 17:22:53 +0100

    Import

---
+++ src/se/exertusit/inkognito/ShakeFragment.java
@@ -0,0 +1,17 @@
+package se.exertusit.inkognito;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+public class ShakeFragment extends Fragment{
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.shake_fragment,
+				container, false);
+		
+		return rootView;
+	}
+}
